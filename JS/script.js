655cf2d879ff8a3efeb5196ab0cf9e77d14792da
filbetka <filbetka@gmail.com>
2019-10-08 00:41:30 +0200

    Created main page

--- JS/script.js
+++ JS/script.js
@@ -15,5 +15,16 @@ function update_product_page(data)
     document.getElementById('json').innerHTML = html;
 }
 
+function update_main_page(data)
+{
+    console.log(data);
+
+    let html = '';
+    html += '<h1>' + data.title + '</h1>';
+    html += '<p>' + data.description + '</p>';
+    document.getElementById('json').innerHTML = html;
+}
+
 let server = Server();
 server.request(update_product_page, 'products_page');
+//server.request(update_main_page, 'main_page');
