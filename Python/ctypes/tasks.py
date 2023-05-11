ceb4de9d86c87b2cbb94484a19933346c629d222
Filip Marlewski <fbetka@sii.pl>
2023-05-11 13:39:07 +0200

    Modified tasks for ctypes.

--- Python/ctypes/tasks.py
+++ Python/ctypes/tasks.py
@@ -1,14 +1,17 @@
 from invoke import task
 
 @task
-def clean_cmult(c):
+def clean(c):
     c.run('rm libcmult.so')
+    print('cleaned solution')
 
 @task
-def build_cmult(c):
+def build(c):
     c.run('gcc -shared -o libcmult.so cmult.c')
+    print('builded libcmult.so')
 
 @task
-def test_ctypes(c):
+def test(c):
     c.run('python ctypes_test.py')
+    print('finished test')
 
