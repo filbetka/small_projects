2757794f6055ebb6a2b7b9f9bf3d64d2974176ca
Filip Marlewski <fbetka@sii.pl>
2023-05-16 14:26:42 +0200

    Set path for pre-commit and call it.

--- Python/django/marketplace/tasks.py
+++ Python/django/marketplace/tasks.py
@@ -1,5 +1,6 @@
-from invoke import task
 import os
+
+from invoke import task
 
 
 def venv_run(c, command: str):
@@ -11,7 +12,8 @@ def start(c):
     if not os.path.exists('venv'):
         c.run('sudo apt-get install -y python3.9-venv')
         c.run('python3.9 -m venv venv')
-        venv_run(c, 'pip install -r requirements.txt')
+        venv_run(c, 'pip install pip-tools')
+        venv_run(c, 'pip-sync')
 
 
 @task
@@ -23,3 +25,8 @@ def run(c):
 def db(c):
     venv_run(c, 'python manage.py makemigrations')
     venv_run(c, 'python manage.py migrate')
+
+
+@task
+def precommit(c):
+    venv_run(c, 'pre-commit run --all-files')
